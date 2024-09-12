f0b89f35608f3480393d8426aeece10546f4d4a4
Ayden Boyko <ajb7196@rit.edu>
2024-09-12 14:39:45 -0400

    facial tracking

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
+
 import os
 import sys
 import cv2
-import numpy as np
-from tarfile import TarFile
-from urllib.request import urlretrieve
 
 # * custom imports
-from Facial_Tracking.draw import drawRectangle, drawText
-from Facial_Tracking.tracker import createTrackerByName
-
-# model path
-modelPath = '/20180408-102900/20180408-102900.pb'
+from draw import drawRectangle, drawText
+from tracker import createTrackerByName
 
 # Set up tracker types
 tracker_types = [
@@ -35,33 +30,25 @@ source = cv2.VideoCapture(s)
 win_name = 'Camera Preview'
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
-# initialize object detection tracker, there are multiple types
-tracker = createTrackerByName(tracker_types[3])
-
-init = False
+print(f'\n {os.getcwd()} \n')
 
 # load object detection dnn from pre-trained model
-# net = cv2.dnn.readNetFromTensorflow('ssd_mobilenet_v1_coco_2017_11_17.pb', 'ssd_mobilenet_v1_coco_2017_11_17.pbtxt')
+net = cv2.dnn.readNetFromCaffe(
+    'Facial_Tracking/models/object_detection/deploy.prototxt',
+    'Facial_Tracking/models/object_detection/res10_300x300_ssd_iter_140000_fp16.caffemodel'
+)
 
-# width
-width = 300
-# height
-height = 300
-# mean
-mean = [0,0,0]
-# confidence
-conf = .7   
+# in_width and in_height are the dimensions of the input image
+in_width = 300
+in_height = 300
+# mean is a list of 3 values representing the mean RGB values of the input image
+mean = [104, 117, 123]
+# conf_threshold is the confidence threshold for the model's output
+conf_threshold = 0.7
 
 # loop through frames until ESC is pressed
 while cv2.waitKey(1) != 27: # Escape
     has_frame, frame = source.read()
-
-    if not init:
-        # TODO: instead of having the user draw a bounding box around their face, use facial tracking instead
-        # TODO  That way if the object detection efver fails the facial detection will kick in again and the face will be tracked again.
-        init = True
-        bbox = cv2.selectROI(win_name, frame)
-        tracker.init(frame, bbox)
 
     if not has_frame:
         break
@@ -69,22 +56,47 @@ while cv2.waitKey(1) != 27: # Escape
     # Start timer
     timer = cv2.getTickCount()
 
-    # Update tracker
-    ok, bbox = tracker.update(frame)
+    frame= cv2.flip(frame, 1)
+
+    # Get frame width and height
+    frame_height = frame.shape[0]
+    frame_width = frame.shape[1]
+
+    # Create a 4D blob from frame
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (in_width, in_height), mean, swapRB=False, crop=False)
+
+    # Run the model
+    net.setInput(blob)
+    detections = net.forward()
 
     # Calculate Frames per second (FPS)
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
 
-    # Draw bounding box
-    if ok:
-        drawRectangle(frame, bbox)
-    else:
-        drawText(frame, "Tracking failure detected", (80, 140), (0, 0, 255))
-        # TODO: if failing, use facial detection to start backup again
+    # loop through detections
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
 
-    # Display Info
-    drawText(frame, str(tracker) + " Tracker", (80, 60))
-    drawText(frame, "FPS : " + str(int(fps)), (80, 100))
+        if confidence > conf_threshold:
+            x_left_bottom = int(detections[0, 0, i, 3] * frame_width)
+            y_left_bottom = int(detections[0, 0, i, 4] * frame_height)
+            x_right_top = int(detections[0, 0, i, 5] * frame_width)
+            y_right_top = int(detections[0, 0, i, 6] * frame_width)
+
+            # draw rectangle
+            cv2.rectangle(frame, (x_left_bottom, y_left_bottom), (x_right_top, y_right_top), (0, 255, 0))
+
+            # draw text
+            label = "Confidence: %.4f" % confidence
+            label_size, baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+            cv2.rectangle(frame, (x_left_bottom, y_left_bottom - label_size[1]), (x_left_bottom + label_size[0], y_left_bottom + baseline), (255, 255, 255), cv2.FILLED)
+            cv2.putText(frame, label, (x_left_bottom, y_left_bottom), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+
+    # Display FPS on frame
+    t, _ = net.getPerfProfile()
+    label = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
+    cv2.putText(frame, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
 
 
     cv2.imshow(win_name, frame)
---
+++ model_extractor.py
@@ -0,0 +1,21 @@
+# ========================-Downloading Assets-========================
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+
+
+def download_and_unzip(url, save_path):
+    print(f"Downloading and extracting assests....", end="")
+
+    # Downloading zip file using urllib package.
+    urlretrieve(url, save_path)
+
+    try:
+        # Extracting zip file using the zipfile package.
+        with ZipFile(save_path) as z:
+            # Extract ZIP file contents in the same directory.
+            z.extractall(os.path.split(save_path)[0])
+
+        print("Done")
+
+    except Exception as e:
+        print("\nInvalid file.", e)
